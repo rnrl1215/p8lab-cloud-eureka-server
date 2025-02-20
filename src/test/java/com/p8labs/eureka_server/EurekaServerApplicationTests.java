3769a61e877684725382fb3e715f54bba937d021
skahn <d1215@naver.com>
2025-02-20 22:30:04 +0900

    "first commit"

--- src/test/java/com/p8labs/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/com/p8labs/eureka_server/EurekaServerApplicationTests.java
@@ -1,8 +1,10 @@
 package com.p8labs.eureka_server;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class EurekaServerApplicationTests {
 
